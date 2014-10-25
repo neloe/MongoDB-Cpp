077f16e39bbfbebaa31f4626bfef299a1d77da05
Nathan Eloe <powerofazure@gmail.com>
2014-10-25 11:36:10 -0500

    add lgpl2.1 license header to all source files

--- connection/mongoclient.h
+++ connection/mongoclient.h
@@ -1,3 +1,20 @@
+/*
+  Copyright (c) Nathan Eloe, 2014
+  This file is part of MongoDB-Cpp.
+
+  MongoDB-Cpp is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  MongoDB-Cpp is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with MongoDB-Cpp.  If not, see <http://www.gnu.org/licenses/>.
+*/
 /*!
  * \file basic_connection.h
  * \author Nathan Eloe
